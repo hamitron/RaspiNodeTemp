ff9ab34c19905a043f98c8a8d10d9d26195c4e28
hamitron <hqkarl@gmail.com>
2015-09-11 21:49:09 -0700

    prints to web template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var PythonShell = require('python-shell');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var pyshell = new PythonShell('faketemp.py');
+	
+	pyshell.send('');
+	
+	var outputmsg;
+	pyshell.on('message', function(message){
+		console.log(message);
+		outputmsg = message;
+		res.render('index', { title: outputmsg });
+	});
+
+  
 });
 
 module.exports = router;
